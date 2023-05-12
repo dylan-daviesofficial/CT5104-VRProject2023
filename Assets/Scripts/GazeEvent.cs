b9c5f5e5bf3a8ca1ce7825eaa5eb9e50f31874b3
dylan-daviesofficial <93916281+dylan-daviesofficial@users.noreply.github.com>
2023-05-12 11:23:48 +0100

    Cube now loads between scenes when looked at for 3 seconds

--- Assets/Scripts/GazeEvent.cs
+++ Assets/Scripts/GazeEvent.cs
@@ -11,7 +11,32 @@ public class GazeEvent : MonoBehaviour
 {
 
     public GEvent onLook;
+    public GEvent onTimerFinish;
+
+    float timer = 0;
+    public float lookTime = 3;
+    bool lookedThisFrame = false;
+
+    public void LateUpdate()
+    {
+        if (lookedThisFrame)
+        {
+            timer += Time.deltaTime;
+            if (timer > lookTime) onTimerFinish.Invoke();
+
+        }
+        else timer = 0;
+        lookedThisFrame = false;
+
+    }
     
+    public void GazingThisFrame()
+    {
+        lookedThisFrame = true;
+    }
     
-    
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
 }
