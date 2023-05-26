ec88f3260cc9a8887709d681ad7a54ac10ca5721
dylan-daviesofficial <93916281+dylan-daviesofficial@users.noreply.github.com>
2023-05-26 18:06:14 +0100

    Changed angle of happy scene

---
+++ VRClimateChange_s4101141_V4_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,197 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void FadeParticleTransitioner::Update()
+extern void FadeParticleTransitioner_Update_m254D4B452054B279814A24177F104D1B4DD72A09 (void);
+// 0x00000002 System.Void FadeParticleTransitioner::ChangePosition()
+extern void FadeParticleTransitioner_ChangePosition_m0810E31073503D1C5B1032E65467ED36823ECED9 (void);
+// 0x00000003 System.Void FadeParticleTransitioner::.ctor()
+extern void FadeParticleTransitioner__ctor_m640E2EE4F79A8B2D16D36F087E07741C99A2BECC (void);
+// 0x00000004 System.Void GEvent::.ctor()
+extern void GEvent__ctor_mEB07AFCF7B07A42A35BDA2C39B708B175502402F (void);
+// 0x00000005 System.Void GazeEvent::LateUpdate()
+extern void GazeEvent_LateUpdate_m15F4C13A34AC877AE90566DE5D3D24C63D354428 (void);
+// 0x00000006 System.Void GazeEvent::GazingThisFrame()
+extern void GazeEvent_GazingThisFrame_mF46E0766811146D1D979FF4234DA74E65E8BFB75 (void);
+// 0x00000007 System.Void GazeEvent::ChangeScene(System.String)
+extern void GazeEvent_ChangeScene_mC5AD6E18B6E96D37DDECB915F1E21E2F457C032D (void);
+// 0x00000008 System.Void GazeEvent::.ctor()
+extern void GazeEvent__ctor_m4CB8EF30F35846612614666499547253DC7E0DD7 (void);
+// 0x00000009 System.Void GazeObject::Update()
+extern void GazeObject_Update_m661F358E41A4AB5B2FBED9A5598F0EEBBEA77F50 (void);
+// 0x0000000A System.Void GazeObject::.ctor()
+extern void GazeObject__ctor_mB797C2923CC1E77D2EE74ADEB80E76C90D1C8904 (void);
+// 0x0000000B System.Void GazeRaycast::Start()
+extern void GazeRaycast_Start_m04DFC7A31E4DDC9527989F309BC842CA2E06A903 (void);
+// 0x0000000C System.Void GazeRaycast::Update()
+extern void GazeRaycast_Update_mE9A48183E7F67465FECA38331D3FECA0255E872C (void);
+// 0x0000000D System.Void GazeRaycast::.ctor()
+extern void GazeRaycast__ctor_m6A7D0B9C44BBBAF4951E7C5726854A7CFF40291C (void);
+// 0x0000000E System.Void GyroMovement::Update()
+extern void GyroMovement_Update_m03097E366E53F2B31529F505AE558226B1C8B540 (void);
+// 0x0000000F System.Collections.IEnumerator GyroMovement::Start()
+extern void GyroMovement_Start_mE7119BCED40C29100C10E9E879BF4403E3869C70 (void);
+// 0x00000010 System.Collections.IEnumerator GyroMovement::CalibrateYAngle()
+extern void GyroMovement_CalibrateYAngle_m09D6232AD7899DF7A3DA8A27D9E4B1F309963D82 (void);
+// 0x00000011 System.Void GyroMovement::ApplyGyroRotation()
+extern void GyroMovement_ApplyGyroRotation_mF54E15FEE5C75C79725057EC65BE4A201C2E2F2B (void);
+// 0x00000012 System.Void GyroMovement::ApplyCalibration()
+extern void GyroMovement_ApplyCalibration_m0815505980C98CC5261C51C2FCCC18A5089738F8 (void);
+// 0x00000013 System.Void GyroMovement::SetEnabled(System.Boolean)
+extern void GyroMovement_SetEnabled_m5057BC80CA74220CE4889EFB575962EAD4742D5B (void);
+// 0x00000014 System.Void GyroMovement::.ctor()
+extern void GyroMovement__ctor_mA9EE3893248C5BCC084984A6D04FBB60151B8A1F (void);
+// 0x00000015 System.Void GyroMovement/<Start>d__11::.ctor(System.Int32)
+extern void U3CStartU3Ed__11__ctor_mAEB6A76C674B8E53B4D260152254FE0CBBC8ACB0 (void);
+// 0x00000016 System.Void GyroMovement/<Start>d__11::System.IDisposable.Dispose()
+extern void U3CStartU3Ed__11_System_IDisposable_Dispose_m704E8EDEF65A654A2408880B4A08BF8B6DA55003 (void);
+// 0x00000017 System.Boolean GyroMovement/<Start>d__11::MoveNext()
+extern void U3CStartU3Ed__11_MoveNext_m8B9570454573900EE3FAF5CD3F5DCE554043AAB0 (void);
+// 0x00000018 System.Object GyroMovement/<Start>d__11::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CStartU3Ed__11_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mA690698FCAA1B87ED1C3B27CA411082DE355C38A (void);
+// 0x00000019 System.Void GyroMovement/<Start>d__11::System.Collections.IEnumerator.Reset()
+extern void U3CStartU3Ed__11_System_Collections_IEnumerator_Reset_m91042309514FE74A9E2D62ED97C02F0D4B2830DD (void);
+// 0x0000001A System.Object GyroMovement/<Start>d__11::System.Collections.IEnumerator.get_Current()
+extern void U3CStartU3Ed__11_System_Collections_IEnumerator_get_Current_m4B8F15FC4B8B1B03EE81FF0688E07BE20C2FA610 (void);
+// 0x0000001B System.Void GyroMovement/<CalibrateYAngle>d__12::.ctor(System.Int32)
+extern void U3CCalibrateYAngleU3Ed__12__ctor_mA0DB17B7BC0127C29D0369745824D2CC68C799AF (void);
+// 0x0000001C System.Void GyroMovement/<CalibrateYAngle>d__12::System.IDisposable.Dispose()
+extern void U3CCalibrateYAngleU3Ed__12_System_IDisposable_Dispose_m1422B1C79E50B300F0E56A9A6A17F68FA9A0463C (void);
+// 0x0000001D System.Boolean GyroMovement/<CalibrateYAngle>d__12::MoveNext()
+extern void U3CCalibrateYAngleU3Ed__12_MoveNext_mB7C1D0A9F95FB969680E9AD9174ECAAA69D52B6D (void);
+// 0x0000001E System.Object GyroMovement/<CalibrateYAngle>d__12::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CCalibrateYAngleU3Ed__12_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mD074E6D5F41A7EA701F3BC2FD69B973BB75B967F (void);
+// 0x0000001F System.Void GyroMovement/<CalibrateYAngle>d__12::System.Collections.IEnumerator.Reset()
+extern void U3CCalibrateYAngleU3Ed__12_System_Collections_IEnumerator_Reset_mEC5B4513A3BBDC5615F391E94BFDA9BD6E8EB624 (void);
+// 0x00000020 System.Object GyroMovement/<CalibrateYAngle>d__12::System.Collections.IEnumerator.get_Current()
+extern void U3CCalibrateYAngleU3Ed__12_System_Collections_IEnumerator_get_Current_m379C3C09B8132E6FDA5CBEB6A12E3E0433BC9C56 (void);
+// 0x00000021 System.Void Gyroscope::Start()
+extern void Gyroscope_Start_mD4B16F7535E5C4E80A24714A7E0641B7F3CC0308 (void);
+// 0x00000022 System.Void Gyroscope::Update()
+extern void Gyroscope_Update_m5E5B6C093DDE41D80536CFDCB6C8A733CC0D15B8 (void);
+// 0x00000023 UnityEngine.Quaternion Gyroscope::GyroToUnity(UnityEngine.Quaternion)
+extern void Gyroscope_GyroToUnity_m495A5A0B1956F983F4BC08FF6F19129E64AC8280 (void);
+// 0x00000024 System.Void Gyroscope::.ctor()
+extern void Gyroscope__ctor_m97AD88BC56F2D0DD98A42C8594801AE229BC3632 (void);
+// 0x00000025 System.Void SC_FPSController::Start()
+extern void SC_FPSController_Start_m4547117A01037F0AF878A8E102CBE820F8F98506 (void);
+// 0x00000026 System.Void SC_FPSController::Update()
+extern void SC_FPSController_Update_m193FDCBD0577912FD1BF3C4FB8C90D55A8ACF2B8 (void);
+// 0x00000027 System.Void SC_FPSController::.ctor()
+extern void SC_FPSController__ctor_mC2C5CA9D19083EE4A1EE106275B30700ACE8103B (void);
+static Il2CppMethodPointer s_methodPointers[39] = 
+{
+	FadeParticleTransitioner_Update_m254D4B452054B279814A24177F104D1B4DD72A09,
+	FadeParticleTransitioner_ChangePosition_m0810E31073503D1C5B1032E65467ED36823ECED9,
+	FadeParticleTransitioner__ctor_m640E2EE4F79A8B2D16D36F087E07741C99A2BECC,
+	GEvent__ctor_mEB07AFCF7B07A42A35BDA2C39B708B175502402F,
+	GazeEvent_LateUpdate_m15F4C13A34AC877AE90566DE5D3D24C63D354428,
+	GazeEvent_GazingThisFrame_mF46E0766811146D1D979FF4234DA74E65E8BFB75,
+	GazeEvent_ChangeScene_mC5AD6E18B6E96D37DDECB915F1E21E2F457C032D,
+	GazeEvent__ctor_m4CB8EF30F35846612614666499547253DC7E0DD7,
+	GazeObject_Update_m661F358E41A4AB5B2FBED9A5598F0EEBBEA77F50,
+	GazeObject__ctor_mB797C2923CC1E77D2EE74ADEB80E76C90D1C8904,
+	GazeRaycast_Start_m04DFC7A31E4DDC9527989F309BC842CA2E06A903,
+	GazeRaycast_Update_mE9A48183E7F67465FECA38331D3FECA0255E872C,
+	GazeRaycast__ctor_m6A7D0B9C44BBBAF4951E7C5726854A7CFF40291C,
+	GyroMovement_Update_m03097E366E53F2B31529F505AE558226B1C8B540,
+	GyroMovement_Start_mE7119BCED40C29100C10E9E879BF4403E3869C70,
+	GyroMovement_CalibrateYAngle_m09D6232AD7899DF7A3DA8A27D9E4B1F309963D82,
+	GyroMovement_ApplyGyroRotation_mF54E15FEE5C75C79725057EC65BE4A201C2E2F2B,
+	GyroMovement_ApplyCalibration_m0815505980C98CC5261C51C2FCCC18A5089738F8,
+	GyroMovement_SetEnabled_m5057BC80CA74220CE4889EFB575962EAD4742D5B,
+	GyroMovement__ctor_mA9EE3893248C5BCC084984A6D04FBB60151B8A1F,
+	U3CStartU3Ed__11__ctor_mAEB6A76C674B8E53B4D260152254FE0CBBC8ACB0,
+	U3CStartU3Ed__11_System_IDisposable_Dispose_m704E8EDEF65A654A2408880B4A08BF8B6DA55003,
+	U3CStartU3Ed__11_MoveNext_m8B9570454573900EE3FAF5CD3F5DCE554043AAB0,
+	U3CStartU3Ed__11_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mA690698FCAA1B87ED1C3B27CA411082DE355C38A,
+	U3CStartU3Ed__11_System_Collections_IEnumerator_Reset_m91042309514FE74A9E2D62ED97C02F0D4B2830DD,
+	U3CStartU3Ed__11_System_Collections_IEnumerator_get_Current_m4B8F15FC4B8B1B03EE81FF0688E07BE20C2FA610,
+	U3CCalibrateYAngleU3Ed__12__ctor_mA0DB17B7BC0127C29D0369745824D2CC68C799AF,
+	U3CCalibrateYAngleU3Ed__12_System_IDisposable_Dispose_m1422B1C79E50B300F0E56A9A6A17F68FA9A0463C,
+	U3CCalibrateYAngleU3Ed__12_MoveNext_mB7C1D0A9F95FB969680E9AD9174ECAAA69D52B6D,
+	U3CCalibrateYAngleU3Ed__12_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mD074E6D5F41A7EA701F3BC2FD69B973BB75B967F,
+	U3CCalibrateYAngleU3Ed__12_System_Collections_IEnumerator_Reset_mEC5B4513A3BBDC5615F391E94BFDA9BD6E8EB624,
+	U3CCalibrateYAngleU3Ed__12_System_Collections_IEnumerator_get_Current_m379C3C09B8132E6FDA5CBEB6A12E3E0433BC9C56,
+	Gyroscope_Start_mD4B16F7535E5C4E80A24714A7E0641B7F3CC0308,
+	Gyroscope_Update_m5E5B6C093DDE41D80536CFDCB6C8A733CC0D15B8,
+	Gyroscope_GyroToUnity_m495A5A0B1956F983F4BC08FF6F19129E64AC8280,
+	Gyroscope__ctor_m97AD88BC56F2D0DD98A42C8594801AE229BC3632,
+	SC_FPSController_Start_m4547117A01037F0AF878A8E102CBE820F8F98506,
+	SC_FPSController_Update_m193FDCBD0577912FD1BF3C4FB8C90D55A8ACF2B8,
+	SC_FPSController__ctor_mC2C5CA9D19083EE4A1EE106275B30700ACE8103B,
+};
+static const int32_t s_InvokerIndices[39] = 
+{
+	4049,
+	4049,
+	4049,
+	4049,
+	4049,
+	4049,
+	3282,
+	4049,
+	4049,
+	4049,
+	4049,
+	4049,
+	4049,
+	4049,
+	3953,
+	3953,
+	4049,
+	4049,
+	3208,
+	4049,
+	3258,
+	4049,
+	3882,
+	3953,
+	4049,
+	3953,
+	3258,
+	4049,
+	3882,
+	3953,
+	4049,
+	3953,
+	4049,
+	4049,
+	2907,
+	4049,
+	4049,
+	4049,
+	4049,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	39,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
